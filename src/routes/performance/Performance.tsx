b268b0ca05fc0d564cf64adb8eec4bef29368da5
MBelniak <belniakm@wp.pl>
2025-08-17 14:00:19 +0200

    Fix build

--- src/routes/performance/Performance.tsx
+++ src/routes/performance/Performance.tsx
@@ -1,4 +1,5 @@
 import { PerformanceChart } from "@/routes/performance/PerformanceChart.tsx";
+import { PerformanceMatrix } from "@/routes/performance/PerformanceMatrix.tsx";
 
 export const Performance = () => {
   return (
